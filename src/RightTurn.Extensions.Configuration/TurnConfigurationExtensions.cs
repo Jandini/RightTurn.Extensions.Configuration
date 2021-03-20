0da09753c3e0459e2842477e8e353eba64320641
Matt Janda <matt.janda.kingston@gmail.com>
2021-03-20 11:00:30 +0000

    feat: Add bind configuration section to singleton service

--- src/RightTurn.Extensions.Configuration/TurnConfigurationExtensions.cs
+++ src/RightTurn.Extensions.Configuration/TurnConfigurationExtensions.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 
 namespace RightTurn.Extensions.Configuration
 {
     public static class TurnConfigurationExtensions
-    {
-        public static ITurn WithConfiguration(this ITurn turn)
-        {
-            turn.Directions.Add<ITurnConfiguration>(new TurnConfiguration());
-            return turn;
-        }
-
+    {     
         public static ITurn WithConfiguration(this ITurn turn, Func<IConfiguration> builder)
         {
             turn.Directions.Add(builder);
@@ -29,5 +24,25 @@ namespace RightTurn.Extensions.Configuration
         public static ITurn WithConfigurationFile(this ITurn turn, string name = "appsettings.json", bool optional = true) => WithConfiguration(turn, (builder) => builder
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile(name, optional));
+
+
+        /// <summary>
+        /// Bind configuration to a service class and add the service as singleton.
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="turn"></param>
+        /// <param name="key">The key of the configuration section to bind.</param>
+        /// <returns></returns>
+        public static ITurn WithConfigurationService<TService, TImplementation>(this ITurn turn, string key) where TService : class where TImplementation : class, TService, new()
+        {
+            var service = new TImplementation();
+            var configuration = turn.Directions.Get<IConfiguration>();
+
+            configuration.Bind(key, service);
+            turn.WithServices(services => services.AddSingleton<TService>(service));
+
+            return turn;
+        }
     }
 }
